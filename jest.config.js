7655ec897cc319953d078a2a1c05201b93fb6f56
Roni Pogrebetski <roni.pogrebetski@tuni.fi>
2025-11-13 12:06:47 +0200

    Add Jest and Babel configurations, add needed packages

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const config = {
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/add.js',
+        'src/filter.js',
+        'src/get.js',
+        'src/isEmpty.js',
+        'src/reduce.js',
+        'src/defaultToAny.js',
+        'src/chunk.js',
+        'src/toNumber.js',
+        'src/eq.js',
+        'src/difference.js',
+    ],
+    };
+
+export default config;
